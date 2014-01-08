d90d17c5c7fba4400914dc4f2b43506e07fb4abc
nclegg <nigelclegg65@gmail.com>
2014-01-08 14:40:06 -0800

    updated client and server

--- assignments/session01/echo_client.py
+++ assignments/session01/echo_client.py
@@ -4,31 +4,30 @@ import sys
 
 def client(msg, log_buffer=sys.stderr):
     server_address = ('localhost', 10000)
-    # TODO: Replace the following line with your code which will instantiate 
-    #       a TCP socket with IPv4 Addressing, call the socket you make 'sock'
-    sock = None
-    print >>log_buffer, 'connecting to {0} port {1}'.format(*server_address)
-    # TODO: connect your socket to the server here.
 
-    # this try/finally block exists purely to allow us to close the socket
-    # when we are finished with it
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP) 
+
+    print >>log_buffer, 'connecting to {0} port {1}'.format(*server_address)
+    sock.connect(server_address)
+
     try:
         print >>log_buffer, 'sending "{0}"'.format(msg)
-        # TODO: send your message to the server here.
 
-        # TODO: the server should be sending you back your message as a series
-        #       of 16-byte chunks.  You will want to log them as you receive 
-        #       each one.  You will also need to check to make sure that 
-        #       you have received the entire message you sent __before__ 
-        #       closing the socket. 
-        # 
-        #       Make sure that you log each chunk you receive.  Use the print 
-        #       statement below to do it. (The tests expect this log format)
-        chunk = ''
-        print >>log_buffer, 'received "{0}"'.format(chunk)
+        sock.sendall(msg)
+
+        buffer_size = 16
+        done = False
+
+        while not done:
+            chunk = sock.recv(buffer_size)
+            print >>log_buffer, 'received "{0}"'.format(chunk)  # ORIGINAL
+
+            # test last bytes received
+            if len(chunk) < buffer_size:
+                done = True
+
     finally:
-        # TODO: after you break out of the loop receiving echoed chunks from 
-        #       the server you will want to close your client socket.
+        sock.close()
         print >>log_buffer, 'closing socket'
 
 
